c0bd0c4c0c905da6ce99bbc66474fbbfb3df1990
Alexander Garagatyi <agaragatyi@codenvy.com>
2015-04-20 17:56:47 +0300

    IDEX-2310: add method to get machine/process logs

--- platform-api/che-core-api-machine/src/main/java/org/eclipse/che/api/machine/server/spi/Instance.java
+++ platform-api/che-core-api-machine/src/main/java/org/eclipse/che/api/machine/server/spi/Instance.java
@@ -14,7 +14,6 @@ import org.eclipse.che.api.core.NotFoundException;
 import org.eclipse.che.api.machine.server.MachineException;
 import org.eclipse.che.api.machine.shared.ProjectBinding;
 
-import java.io.File;
 import java.util.List;
 
 /**
@@ -30,6 +29,17 @@ public interface Instance {
 
     List<InstanceProcess> getProcesses() throws MachineException;
 
+    /**
+     * Create process from command line.
+     * Returned {@link InstanceProcess#getPid()} should return unique pid on this stage.
+     * This pid allow to control process from clients and save process logs if needed.
+     *
+     * @param commandLine
+     *         command line from which process should be created
+     * @return {@link InstanceProcess} with unique pid, that can't be used in future for other process of instance
+     * @throws MachineException
+     *         if error occurs on creating process
+     */
     InstanceProcess createProcess(String commandLine) throws MachineException;
 
     ImageKey saveToImage(String owner, String label) throws MachineException;
@@ -39,16 +49,20 @@ public interface Instance {
     /**
      * Binds project to machine instance
      *
-     * @param workspaceId workspace where project is placed
-     * @param project project that should be bound to machine instance
+     * @param workspaceId
+     *         workspace where project is placed
+     * @param project
+     *         project that should be bound to machine instance
      */
     void bindProject(String workspaceId, ProjectBinding project) throws MachineException;
 
     /**
      * Unbinds project from machine instance
      *
-     * @param workspaceId workspace where project is placed
-     * @param project project that should be unbound from machine instance
+     * @param workspaceId
+     *         workspace where project is placed
+     * @param project
+     *         project that should be unbound from machine instance
      */
     void unbindProject(String workspaceId, ProjectBinding project) throws MachineException;
 }
