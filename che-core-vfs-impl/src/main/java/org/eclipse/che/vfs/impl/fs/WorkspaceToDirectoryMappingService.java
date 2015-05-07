1896c6b37b39efe64ae514e670c2571f3ffa8cb6
Valeriy Svydenko <vsvydenko@codenvy.com>
2015-05-07 17:06:33 +0300

    IDEX-2457: improved Workspace location dialog

--- che-core-vfs-impl/src/main/java/org/eclipse/che/vfs/impl/fs/WorkspaceToDirectoryMappingService.java
+++ che-core-vfs-impl/src/main/java/org/eclipse/che/vfs/impl/fs/WorkspaceToDirectoryMappingService.java
@@ -18,6 +18,7 @@ import org.eclipse.che.api.vfs.server.VirtualFileSystemProvider;
 import org.eclipse.che.api.vfs.server.VirtualFileSystemRegistry;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -31,6 +32,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -44,6 +46,10 @@ public class WorkspaceToDirectoryMappingService {
     @Inject
     private VirtualFileSystemRegistry virtualFileSystemRegistry;
 
+    @Inject
+    @Named("vfs.local.fs_root_dir")
+    String rootDir;
+
     @POST
     @Path("{ws-id}")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -51,13 +57,13 @@ public class WorkspaceToDirectoryMappingService {
     public Map<String, String> setMountPath(@PathParam("ws-id") String workspaceId, @QueryParam("mountPath") String mountPath)
             throws ServerException, IOException {
         if (Files.notExists(Paths.get(mountPath))) {
-            throw new ServerException(String.format("Folder %s not found ", mountPath));
-        } else {
-            VirtualFileSystemProvider provider = virtualFileSystemRegistry.getProvider(workspaceId);
-            provider.close();
-            mappedDirectoryLocalFSMountStrategy.setMountPath(workspaceId, new File(mountPath));
-            return getDirectoryMapping();
+            Files.createDirectories(Paths.get(mountPath));
         }
+
+        VirtualFileSystemProvider provider = virtualFileSystemRegistry.getProvider(workspaceId);
+        provider.close();
+        mappedDirectoryLocalFSMountStrategy.setMountPath(workspaceId, new File(mountPath));
+        return getDirectoryMapping();
     }
 
     @DELETE
@@ -74,12 +80,21 @@ public class WorkspaceToDirectoryMappingService {
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Map<String, String> getDirectoryMapping() throws IOException {
-        return Maps.transformValues(mappedDirectoryLocalFSMountStrategy.getDirectoryMapping(), new Function<File, String>() {
-            @Override
-            public String apply(File input) {
-                return input.getAbsolutePath();
-            }
-        });
+    public Map<String, String> getDirectoryMapping() throws IOException, ServerException {
+        Map<String, String> directoryMapping;
+        if (mappedDirectoryLocalFSMountStrategy.getDirectoryMapping().isEmpty()) {
+            directoryMapping = new HashMap<>();
+            directoryMapping.put("default", rootDir);
+            return directoryMapping;
+        }
+        directoryMapping =
+                Maps.transformValues(mappedDirectoryLocalFSMountStrategy.getDirectoryMapping(), new Function<File, String>() {
+                    @Override
+                    public String apply(File input) {
+                        return input.getAbsolutePath();
+                    }
+                });
+
+        return directoryMapping;
     }
 }
