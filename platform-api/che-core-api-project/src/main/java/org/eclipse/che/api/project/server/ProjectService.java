3f8f45ccd861659f9fe500bce466fff839f9c9c8
Vitaly Parfonov <vparfonov@codenvy.com>
2015-05-12 16:21:08 +0300

    IDEX-2521

--- platform-api/che-core-api-project/src/main/java/org/eclipse/che/api/project/server/ProjectService.java
+++ platform-api/che-core-api-project/src/main/java/org/eclipse/che/api/project/server/ProjectService.java
@@ -347,10 +347,10 @@ public class ProjectService extends Service {
             throws NotFoundException, ConflictException, ForbiddenException, ServerException {
 
         Project module = projectManager.addModule(workspace, parentPath, path,
-                                                  (newProject == null) ? null : DtoConverter
-                                                          .fromDto2(newProject, projectManager.getProjectTypeRegistry()),
-                                                  (newProject == null) ? null : newProject.getGeneratorDescription().getOptions(),
-                                                  (newProject == null) ? null : newProject.getVisibility());
+                (newProject == null) ? null : DtoConverter
+                        .fromDto2(newProject, projectManager.getProjectTypeRegistry()),
+                (newProject == null) ? null : newProject.getGeneratorDescription().getOptions(),
+                (newProject == null) ? null : newProject.getVisibility());
 
 
         final ProjectDescriptor descriptor = DtoConverter.toDescriptorDto2(module, getServiceContext().getServiceUriBuilder(),
@@ -763,7 +763,7 @@ public class ProjectService extends Service {
             final String projectType = project.getConfig().getTypeId();
             entry.remove();
             LOG.info("EVENT#project-destroyed# PROJECT#{}# TYPE#{}# WS#{}# USER#{}#", name, projectType,
-                     EnvironmentContext.getCurrent().getWorkspaceName(), EnvironmentContext.getCurrent().getUser().getName());
+                    EnvironmentContext.getCurrent().getWorkspaceName(), EnvironmentContext.getCurrent().getUser().getName());
 
             logProjectCreatedEvent(name, projectType);
         }
@@ -885,6 +885,15 @@ public class ProjectService extends Service {
         try {
             if (newProject != null) {
                 visibility = newProject.getVisibility();
+
+                //TODO:
+                //this trick need for fixing problem for default runners in case
+                //scripts downloaded  from remote resources "docker" this is only marker not real path in file system
+                //see importRunnerEnvironment() method
+                if (newProject.getRunners() != null && newProject.getRunners().getDefault() != null && newProject.getRunners().getDefault().startsWith("project:/docker/")) {
+                    String replace = newProject.getRunners().getDefault().replace("project:/docker/", "project:/");
+                    newProject.getRunners().setDefault(replace);
+                }
                 projectConfig = DtoConverter.fromDto2(newProject, projectTypeRegistry);
             }
             project = projectManager.convertFolderToProject(workspace,
