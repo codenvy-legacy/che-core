bb8ebd80d84f619c0d3b4efc054c5664efaec087
Vitaly Parfonov <vparfonov@codenvy.com>
2015-06-12 13:22:35 +0300

    merge with master

--- ide/che-core-ide-api/src/test/java/org/eclipse/che/ide/api/project/tree/generic/GenericTreeStructureTest.java
+++ ide/che-core-ide-api/src/test/java/org/eclipse/che/ide/api/project/tree/generic/GenericTreeStructureTest.java
@@ -992,7 +992,7 @@ public class GenericTreeStructureTest {
         verify(projectNode).refreshChildren(treeNodeAsyncCallback.capture());
         treeNodeAsyncCallback.getValue().onSuccess(projectNode);
 
-        verify(asyncCallback).onSuccess(null);
+        verify(asyncCallback).onFailure(any(IllegalStateException.class));
     }
 
     @Test
