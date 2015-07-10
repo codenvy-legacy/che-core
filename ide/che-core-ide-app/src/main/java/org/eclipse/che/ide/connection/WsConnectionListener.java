df48a98a5e9dfae0299d007ff6c8babcea098768
Roman Nikitenko <rnikitenko@codenvy.com>
2015-07-10 13:59:40 +0300

    IDEX-2350. Added ability to handle each status code when a connection was closed. Show message dialog for Che assembly and prompt to re-login for hosted env.

--- ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/connection/WsConnectionListener.java
+++ ide/che-core-ide-app/src/main/java/org/eclipse/che/ide/connection/WsConnectionListener.java
@@ -10,32 +10,51 @@
  *******************************************************************************/
 package org.eclipse.che.ide.connection;
 
+import org.eclipse.che.ide.api.ConnectionClosedInformer;
 import org.eclipse.che.ide.api.event.HttpSessionDestroyedEvent;
+import org.eclipse.che.ide.util.loging.Log;
 import org.eclipse.che.ide.websocket.MessageBus;
 import org.eclipse.che.ide.websocket.events.ConnectionClosedHandler;
+import org.eclipse.che.ide.websocket.events.ConnectionOpenedHandler;
 import org.eclipse.che.ide.websocket.events.WebSocketClosedEvent;
+
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 
 /**
  * @author Evgen Vidolob
  */
-public class WsConnectionListener implements ConnectionClosedHandler {
+public class WsConnectionListener implements ConnectionClosedHandler, ConnectionOpenedHandler {
 
 
-    private EventBus eventBus;
+    private EventBus                 eventBus;
+    private MessageBus               messageBus;
+    private ConnectionClosedInformer connectionClosedInformer;
 
     @Inject
-    public WsConnectionListener(MessageBus messageBus, EventBus eventBus) {
+    public WsConnectionListener(MessageBus messageBus,
+                                EventBus eventBus,
+                                ConnectionClosedInformer connectionClosedInformer) {
         this.eventBus = eventBus;
+        this.messageBus = messageBus;
+        this.connectionClosedInformer = connectionClosedInformer;
         messageBus.addOnCloseHandler(this);
     }
 
     @Override
     public void onClose(WebSocketClosedEvent event) {
+        messageBus.removeOnCloseHandler(this);
+        Log.info(getClass(), "WebSocket is closed, the status code is " + event.getCode() + ", the reason is " + event.getReason());
+
         if (event.getCode() == WebSocketClosedEvent.CLOSE_NORMAL && "Http session destroyed".equals(event.getReason())) {
             eventBus.fireEvent(new HttpSessionDestroyedEvent());
+            return;
         }
+        connectionClosedInformer.onConnectionClosed(event);
     }
 
+    @Override
+    public void onOpen() {
+        messageBus.addOnCloseHandler(this);
+    }
 }
