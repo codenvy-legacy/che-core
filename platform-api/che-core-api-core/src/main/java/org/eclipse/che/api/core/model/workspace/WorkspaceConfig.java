010bfc3a5ead67d114c9225f1a92402b5dc0f519
Eugene Voevodin <evoevodin@codenvy.com>
2015-08-04 12:33:35 +0300

    added workspace cfg javadoc

--- platform-api/che-core-api-core/src/main/java/org/eclipse/che/api/core/model/workspace/WorkspaceConfig.java
+++ platform-api/che-core-api-core/src/main/java/org/eclipse/che/api/core/model/workspace/WorkspaceConfig.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Codenvy, S.A. - initial API and implementation
+ * Codenvy, S.A. - initial API and implementation
  *******************************************************************************/
 package org.eclipse.che.api.core.model.workspace;
 
@@ -16,21 +16,52 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Defines workspace configuration.
+ *
  * @author gazarenkov
  */
 public interface WorkspaceConfig {
 
+    /**
+     * Returns workspace name.
+     * The name is unique, mandatory and updatable
+     */
     String getName();
 
+    /**
+     * Returns default environment name.
+     * It is mandatory and updatable, implementation should guarantee that environment
+     * with returned name exists for current workspace config
+     */
     String getDefaultEnvironment();
 
+    /**
+     * Returns commands which are related to workspace,
+     * when workspace doesn't contain commands returns empty list.
+     * It is optional and updatable, workspace may contain 0 or N commands.
+     */
     List<? extends Command> getCommands();
 
+    /**
+     * Returns project configurations which are related to workspace,
+     * when workspace doesn't contain commands returns empty list.
+     * It is optional and updatable, workspace may contain 0 or N project configurations
+     */
     List<? extends ProjectConfig> getProjects();
 
+    /**
+     * Returns workspace environments.
+     * Workspace must contain at least 1 default environment and may contain N environments, it is updatable
+     */
     Map<String, ? extends Environment> getEnvironments();
 
-    Environment getEnvironment(String envId);
+    //TODO should it be present in model interface?
+    Environment getEnvironment(String envName);
 
+    /**
+     * Returns workspace attributes, if workspace doesn't have any attributes empty map will be returned.
+     * Workspace attribute is an additional information about workspace which may be stored as key-value.
+     * It is optional and updatable, workspace may contain 0 or N attributes
+     */
     Map<String, String> getAttributes();
 }
