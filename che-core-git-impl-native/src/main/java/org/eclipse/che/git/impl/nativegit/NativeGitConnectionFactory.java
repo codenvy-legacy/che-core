57b475721e3e8b45646fef040464830292a05729
Valeriy Svydenko <vsvydenko@codenvy.com>
2015-08-28 16:30:48 +0300

    added an ability to save git committer name and email into user preferences

--- che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/NativeGitConnectionFactory.java
+++ che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/NativeGitConnectionFactory.java
@@ -10,48 +10,38 @@
  *******************************************************************************/
 package org.eclipse.che.git.impl.nativegit;
 
-import org.eclipse.che.api.core.NotFoundException;
-import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.util.LineConsumerFactory;
 import org.eclipse.che.api.git.GitConnection;
 import org.eclipse.che.api.git.GitConnectionFactory;
 import org.eclipse.che.api.git.GitException;
 import org.eclipse.che.api.git.shared.GitUser;
-import org.eclipse.che.api.user.server.dao.UserProfileDao;
-import org.eclipse.che.commons.env.EnvironmentContext;
-import org.eclipse.che.commons.user.User;
 import org.eclipse.che.dto.server.DtoFactory;
-
-import com.google.common.base.Joiner;
-
 import org.eclipse.che.git.impl.nativegit.ssh.GitSshScriptProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.che.ide.api.preferences.PreferencesManager;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.File;
-import java.util.Map;
 
 /**
  * Native implementation for GitConnectionFactory
  *
  * @author Eugene Voevodin
+ * @author Valeriy Svydenko
  */
 @Singleton
 public class NativeGitConnectionFactory extends GitConnectionFactory {
 
-    private static final Logger LOG = LoggerFactory.getLogger(NativeGitConnectionFactory.class);
-
     private final CredentialsLoader    credentialsLoader;
-    private final UserProfileDao       userProfileDao;
     private final GitSshScriptProvider gitSshScriptProvider;
+    private final PreferencesManager   preferencesManager;
 
     @Inject
-    public NativeGitConnectionFactory(CredentialsLoader credentialsLoader, GitSshScriptProvider gitSshScriptProvider,
-                                      UserProfileDao userProfileDao) {
+    public NativeGitConnectionFactory(CredentialsLoader credentialsLoader,
+                                      GitSshScriptProvider gitSshScriptProvider,
+                                      PreferencesManager   preferencesManager) {
         this.credentialsLoader = credentialsLoader;
-        this.userProfileDao = userProfileDao;
+        this.preferencesManager = preferencesManager;
         this.gitSshScriptProvider = gitSshScriptProvider;
     }
 
@@ -71,29 +61,14 @@ public class NativeGitConnectionFactory extends GitConnectionFactory {
 
 
     private GitUser getGitUser() {
-        final User user = EnvironmentContext.getCurrent().getUser();
-        Map<String, String> profileAttributes = null;
-        try {
-            profileAttributes = userProfileDao.getById(user.getId()).getAttributes();
-        } catch (NotFoundException | ServerException e) {
-            LOG.warn("Failed to obtain user information.", e);
-        }
         final GitUser gitUser = DtoFactory.getInstance().createDto(GitUser.class);
-        if (profileAttributes == null) {
-            return gitUser.withName(user.getName()).withEmail("anonymous@noemail.com");
-        }
-        final String firstName = profileAttributes.get("firstName");
-        final String lastName = profileAttributes.get("lastName");
-        final String email = profileAttributes.get("email");
-        String name;
-        if (firstName != null || lastName != null) {
-            // add this temporary for fixing problem with "<none>" in last name of user from profile
-            name = Joiner.on(" ").skipNulls().join(firstName, lastName.contains("<none>") ? "" : lastName);
-        } else {
-            name = user.getName();
-        }
+
+        String name = preferencesManager.getValue("git.committer.name");
+        String email = preferencesManager.getValue("git.committer.email");
+
         gitUser.setName(name != null && !name.isEmpty() ? name : "Anonymous");
         gitUser.setEmail(email != null ? email : "anonymous@noemail.com");
+
         return gitUser;
     }
 }
