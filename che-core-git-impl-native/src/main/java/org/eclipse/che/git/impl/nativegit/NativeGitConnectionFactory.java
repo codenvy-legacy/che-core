83a63fee57769aac18c0c6aa13fc9f8f73d6e275
Valeriy Svydenko <vsvydenko@codenvy.com>
2015-09-02 11:59:03 +0300

    IDEX-2180: code clean up

--- che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/NativeGitConnectionFactory.java
+++ che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/NativeGitConnectionFactory.java
@@ -10,18 +10,23 @@
  *******************************************************************************/
 package org.eclipse.che.git.impl.nativegit;
 
+import org.eclipse.che.api.core.ServerException;
 import org.eclipse.che.api.core.util.LineConsumerFactory;
 import org.eclipse.che.api.git.GitConnection;
 import org.eclipse.che.api.git.GitConnectionFactory;
 import org.eclipse.che.api.git.GitException;
 import org.eclipse.che.api.git.shared.GitUser;
+import org.eclipse.che.api.user.server.dao.PreferenceDao;
+import org.eclipse.che.commons.env.EnvironmentContext;
 import org.eclipse.che.dto.server.DtoFactory;
 import org.eclipse.che.git.impl.nativegit.ssh.GitSshScriptProvider;
-import org.eclipse.che.ide.api.preferences.PreferencesManager;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.File;
+import java.util.Map;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
  * Native implementation for GitConnectionFactory
@@ -34,15 +39,15 @@ public class NativeGitConnectionFactory extends GitConnectionFactory {
 
     private final CredentialsLoader    credentialsLoader;
     private final GitSshScriptProvider gitSshScriptProvider;
-    private final PreferencesManager   preferencesManager;
+    private PreferenceDao preferenceDao;
 
     @Inject
     public NativeGitConnectionFactory(CredentialsLoader credentialsLoader,
                                       GitSshScriptProvider gitSshScriptProvider,
-                                      PreferencesManager   preferencesManager) {
+                                      PreferenceDao preferenceDao) {
         this.credentialsLoader = credentialsLoader;
-        this.preferencesManager = preferencesManager;
         this.gitSshScriptProvider = gitSshScriptProvider;
+        this.preferenceDao = preferenceDao;
     }
 
     @Override
@@ -63,11 +68,19 @@ public class NativeGitConnectionFactory extends GitConnectionFactory {
     private GitUser getGitUser() {
         final GitUser gitUser = DtoFactory.getInstance().createDto(GitUser.class);
 
-        String name = preferencesManager.getValue("git.committer.name");
-        String email = preferencesManager.getValue("git.committer.email");
+        Map<String, String> preferences;
+        try {
+            preferences = preferenceDao.getPreferences(EnvironmentContext.getCurrent().getUser().getId(), "git.committer.\\w+");
 
-        gitUser.setName(name != null && !name.isEmpty() ? name : "Anonymous");
-        gitUser.setEmail(email != null ? email : "anonymous@noemail.com");
+            String name = preferences.get("git.committer.name");
+            String email = preferences.get("git.committer.email");
+
+            gitUser.setName(isNullOrEmpty(name) ? "Anonymous" : name);
+            gitUser.setEmail(isNullOrEmpty(email) ? "anonymous@noemail.com" : email);
+        } catch (ServerException e) {
+            gitUser.setName("Anonymous");
+            gitUser.setEmail("anonymous@noemail.com");
+        }
 
         return gitUser;
     }
