9b2a1d8383a4cbfbc1905ffee7d17727d8ec0811
Sergey Leschenko <sleschenko@codenvy.com>
2015-09-02 10:44:14 +0300

    IDEX-2712 Added checking of root repository folder

--- che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/NativeGitConnectionFactory.java
+++ che-core-git-impl-native/src/main/java/org/eclipse/che/git/impl/nativegit/NativeGitConnectionFactory.java
@@ -24,6 +24,7 @@ import org.eclipse.che.dto.server.DtoFactory;
 
 import com.google.common.base.Joiner;
 
+import org.eclipse.che.git.impl.nativegit.commands.EmptyGitCommand;
 import org.eclipse.che.git.impl.nativegit.ssh.GitSshScriptProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
