8ebae943774a7460ba5a2495c20752546c8738a3
Max Shaposhnik <mshaposhnik@codenvy.com>
2015-09-08 14:18:00 +0300

    IDEX-2909; add rest method for retrieveing OAuth token;

--- platform-api/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/OAuthAuthenticationService.java
+++ platform-api/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/OAuthAuthenticationService.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package org.eclipse.che.security.oauth;
 
+import org.eclipse.che.api.auth.shared.dto.OAuthToken;
+import org.eclipse.che.api.core.BadRequestException;
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.core.rest.annotations.Required;
 import org.eclipse.che.api.core.rest.shared.dto.Link;
 import org.eclipse.che.api.core.rest.shared.dto.LinkParameter;
 import org.eclipse.che.api.core.util.LinksHelper;
@@ -17,13 +21,14 @@ import org.eclipse.che.security.oauth.shared.dto.OAuthAuthenticatorDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -87,7 +92,7 @@ public class OAuthAuthenticationService {
      */
     @GET
     @Path("authenticate")
-    public Response authenticate() throws OAuthAuthenticationException {
+    public Response authenticate() throws OAuthAuthenticationException, BadRequestException {
         OAuthAuthenticator oauth = getAuthenticator(uriInfo.getQueryParameters().getFirst("oauth_provider"));
         final URL requestUrl = getRequestUrl(uriInfo);
         final List<String> scopes = uriInfo.getQueryParameters().get("scope");
@@ -100,7 +105,7 @@ public class OAuthAuthenticationService {
 
     @GET
     @Path("callback")
-    public Response callback() throws OAuthAuthenticationException {
+    public Response callback() throws OAuthAuthenticationException, BadRequestException {
         URL requestUrl = getRequestUrl(uriInfo);
         Map<String, List<String>> params = getRequestParameters(getState(requestUrl));
         List<String> errorValues = uriInfo.getQueryParameters().get("error");
@@ -226,7 +231,7 @@ public class OAuthAuthenticationService {
 
     @GET
     @Path("invalidate")
-    public Response invalidate() throws IOException {
+    public Response invalidate() throws IOException, BadRequestException {
         final Principal principal = security.getUserPrincipal();
         OAuthAuthenticator oauth = getAuthenticator(uriInfo.getQueryParameters().getFirst("oauth_provider"));
         if (principal != null && oauth.invalidateToken(principal.getName())) {
@@ -236,15 +241,33 @@ public class OAuthAuthenticationService {
                                                                            null)).type(MediaType.TEXT_PLAIN).build();
     }
 
-    protected OAuthAuthenticator getAuthenticator(String oauthProviderName) {
+    /**
+     * Gets OAuth token for user.
+     *
+     * @param oauthProvider OAuth provider name
+     * @return OAuthToken
+     * @throws ServerException
+     */
+    @GET
+    @Path("token")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed({"user", "temp_user"})
+    public OAuthToken token(@Required @QueryParam("oauth_provider") String oauthProvider) throws ServerException, BadRequestException {
+        OAuthAuthenticator provider = getAuthenticator(oauthProvider);
+        try {
+            return provider.getToken(security.getUserPrincipal()
+                                             .getName());
+        } catch (IOException e) {
+            throw new ServerException(e.getLocalizedMessage(), e);
+        }
+    }
+
+    protected OAuthAuthenticator getAuthenticator(String oauthProviderName) throws BadRequestException {
         OAuthAuthenticator oauth = providers.getAuthenticator(oauthProviderName);
         if (oauth == null) {
             LOG.error("Unsupported OAuth provider {} ", oauthProviderName);
-            throw new WebApplicationException(Response.status(400).entity("Unsupported OAuth provider " +
-                                                                          oauthProviderName).type(MediaType.TEXT_PLAIN)
-                                                      .build());
+            throw new BadRequestException("Unsupported OAuth provider " + oauthProviderName);
         }
         return oauth;
     }
-
 }
