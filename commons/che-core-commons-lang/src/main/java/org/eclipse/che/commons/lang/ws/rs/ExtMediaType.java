8b6b170c0fcbfaa9650838b1e6f5d56cf21b959b
Anton Korneta <akorneta@codenvy.com>
2015-09-11 16:42:39 +0300

    IDEX-2881: added api methods for copy resources between machines

--- commons/che-core-commons-lang/src/main/java/org/eclipse/che/commons/lang/ws/rs/ExtMediaType.java
+++ commons/che-core-commons-lang/src/main/java/org/eclipse/che/commons/lang/ws/rs/ExtMediaType.java
@@ -26,5 +26,12 @@ public interface ExtMediaType {
      * A {@link MediaType} constant representing "{@value #APPLICATION_ZIP}" media type.
      */
     public final static MediaType APPLICATION_ZIP_TYPE = new MediaType("application", "zip");
-
+    /**
+     * A {@code String} constant representing "{@value #APPLICATION_X_TAR}" media type.
+     */
+    String APPLICATION_X_TAR = "application/x-tar";
+    /**
+     * A {@link MediaType} constant representing "{@value #APPLICATION_X_TAR}" media type.
+     */
+    MediaType APPLICATION_X_TAR_TYPE = new MediaType("application", "x-tar");
 }
