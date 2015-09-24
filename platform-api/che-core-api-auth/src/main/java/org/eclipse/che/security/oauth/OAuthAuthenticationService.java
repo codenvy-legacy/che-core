8db5f3ff02aea6b8ebb6d1f3cab977bc7fe2433c
Sergii Kabashniuk <skabashnyuk@codenvy.com>
2015-09-24 16:05:23 +0300

    IDEX-3132 check if userid is the same that set in context
    Signed-off-by: Sergii Kabashniuk <skabashnyuk@codenvy.com>

--- platform-api/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/OAuthAuthenticationService.java
+++ platform-api/che-core-api-auth/src/main/java/org/eclipse/che/security/oauth/OAuthAuthenticationService.java
@@ -10,9 +10,12 @@
  *******************************************************************************/
 package org.eclipse.che.security.oauth;
 
+import com.google.common.base.Strings;
+
 import org.eclipse.che.api.core.rest.shared.dto.Link;
 import org.eclipse.che.api.core.rest.shared.dto.LinkParameter;
 import org.eclipse.che.api.core.util.LinksHelper;
+import org.eclipse.che.commons.env.EnvironmentContext;
 import org.eclipse.che.security.oauth.shared.dto.OAuthAuthenticatorDescriptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -92,6 +95,10 @@ public class OAuthAuthenticationService {
         final URL requestUrl = getRequestUrl(uriInfo);
         final List<String> scopes = uriInfo.getQueryParameters().get("scope");
         String userId = uriInfo.getQueryParameters().getFirst("userId");
+        if (!Strings.isNullOrEmpty(userId) && !EnvironmentContext.getCurrent().getUser().getId().equals(userId)) {
+            throw new OAuthAuthenticationException(
+                    "Provided userId " + userId + " is not related to current user " + EnvironmentContext.getCurrent().getUser().getName());
+        }
 
         final String authUrl = oauth.getAuthenticateUrl(requestUrl, userId,
                                                         scopes == null ? Collections.<String>emptyList() : scopes);
