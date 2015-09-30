bc2edb2ffb928bced7aeb3bf63bafb97aeee0857
Artem Zatsarynnyy <azatsarynnyy@codenvy.com>
2015-09-30 15:53:11 +0300

    Workspace service should receive DTO instead of model object for updating ws

--- platform-api/che-core-api-workspace/src/main/java/org/eclipse/che/api/workspace/server/WorkspaceService.java
+++ platform-api/che-core-api-workspace/src/main/java/org/eclipse/che/api/workspace/server/WorkspaceService.java
@@ -142,7 +142,7 @@ public class WorkspaceService extends Service {
     @Path("/{id}/config")
     @Consumes(APPLICATION_JSON)
     @Produces(APPLICATION_JSON)
-    public UsersWorkspaceDto update(@PathParam("id") String id, WorkspaceConfig workspaceCfg)
+    public UsersWorkspaceDto update(@PathParam("id") String id, WorkspaceConfigDto workspaceCfg)
             throws BadRequestException, ServerException, ForbiddenException, NotFoundException, ConflictException {
         return asUsersWorkspaceDto(workspaceManager.updateWorkspace(id, workspaceCfg));
     }
