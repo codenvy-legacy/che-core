4506c1980c762a0d4c5b05879b532cf161181702
Meirav Daniel <meirav.daniel@sap.com>
2015-10-20 06:32:37 +0000

    Add Javadoc comments for RebaseRequest

--- platform-api/che-core-api-git/src/main/java/org/eclipse/che/api/git/shared/RebaseRequest.java
+++ platform-api/che-core-api-git/src/main/java/org/eclipse/che/api/git/shared/RebaseRequest.java
@@ -19,12 +19,41 @@ import org.eclipse.che.dto.shared.DTO;
  */
 @DTO
 public interface RebaseRequest {
-	/** set rebase operation */
-	void setOperation(String operation);
-	/** @return operation used */
-	String getOperation();
-	/** @ return rebase branch */
-	String getBranch();
-	/** set rebase branch */
-	void setBranch(String branch);
+    /** 
+     *  sets the rebase rebase operation 
+     *  can be on of these values:
+     *  
+     *  BEGIN(default) : begin a git rebase operation
+     *  ABORT: abort an ongoing git rebase operation
+     *  CONTINUE: continue with gir rebase operation after conflicts have been resolved
+     *  SKIP: bypass the commit that causes the merge failure
+     *  
+     *  see https://git-scm.com/docs/git-rebase for more information
+     *  
+     * @param The rebase opreation to use
+     */
+    void setOperation(String operation);
+    /** 
+     * gets the rebase rebase operation
+     * 
+     *  @return the operation used in the git request 
+     */
+    String getOperation();
+    /** 
+     * set the rebase branch to use for rebase operation 
+     * May be any valid commit, or an existing branch name.
+     * Defaults to the configured upstream for the current branch.
+     * 
+     * see https://git-scm.com/docs/git-rebase for more information
+     * 
+     * @param The rebase branch to use
+     */
+    void setBranch(String branch);
+    /** 
+     * gets the branch used in rebase operation
+     * 
+     * @return the rebase branch 
+     */
+    String getBranch();
+    
 }
