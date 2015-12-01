3c040adeff53faaa4d0588bf7e0cfd7bbcbd3e42
Sergii Kabashniuk <skabashnyuk@codenvy.com>
2015-12-01 14:27:33 +0200

    Set up general infrastructure of git TCK

---
+++ che-core-git-impl-jgit/src/test/java/org/eclipse/che/git/impl/GitConnectionFactoryProvider.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.git.impl;
+
+import org.eclipse.che.api.core.NotFoundException;
+import org.eclipse.che.api.core.ServerException;
+import org.eclipse.che.api.git.CredentialsLoader;
+import org.eclipse.che.api.user.server.dao.User;
+import org.eclipse.che.api.user.server.dao.UserDao;
+import org.eclipse.che.git.impl.jgit.JGitConnectionFactory;
+import org.testng.annotations.DataProvider;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Sergii Kabashniuk
+ */
+public class GitConnectionFactoryProvider {
+
+    @DataProvider(name = "GitConnectionFactory")
+    public static Object[][] createConnection() throws ServerException, NotFoundException {
+        final UserDao dao = mock(UserDao.class);
+        when(dao.getById(anyString())).thenReturn(new User().withEmail("email@com.com").withId("id"));
+        return new Object[][]{
+                new Object[]{
+                        new JGitConnectionFactory(
+                                mock(CredentialsLoader.class),
+                                dao
+                        )
+                }
+        };
+    }
+}
