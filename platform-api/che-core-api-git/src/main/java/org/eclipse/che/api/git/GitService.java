c68087ebb0389eb0c35a3b5892bfbcffdb15da27
Offer Shostak <offer.shostak@sap.com>
2016-05-12 17:34:13 +0300

    [GIT on Che] Store and Edit GIT Settings (DEVXBABEL-15) - Add new unset config API for Git implementation
    Change-Id: I1243d98063261c5aaab088c149a4f99fee78d7b4

--- platform-api/che-core-api-git/src/main/java/org/eclipse/che/api/git/GitService.java
+++ platform-api/che-core-api-git/src/main/java/org/eclipse/che/api/git/GitService.java
@@ -69,6 +69,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -263,8 +264,8 @@ public class GitService {
         try (GitConnection gitConnection = getGitConnection()) {
     	    return gitConnection.rebase(request);
         }
-    }    
-    
+    }
+
     @Path("mv")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -380,7 +381,6 @@ public class GitService {
 
     @Path("config")
     @GET
-    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Map<String, String> getConfig(@QueryParam("requestedConfig") List<String> requestedConfig)
             throws ApiException {
@@ -420,6 +420,24 @@ public class GitService {
                     final String msg = "Cannot write to config file";
                     LOG.error(msg, exception);
                     throw new GitException(msg);
+                }
+            }
+        }
+    }
+
+    @Path("config")
+    @DELETE
+    public void unsetConfig(@QueryParam("requestedConfig") List<String> requestedConfig)
+            throws ApiException {
+        try (GitConnection gitConnection = getGitConnection()) {
+            Config config = gitConnection.getConfig();
+            if (requestedConfig != null && !requestedConfig.isEmpty()) {
+                for (String entry : requestedConfig) {
+                    try {
+                        config.unset(entry);
+                    } catch (GitException exception) {
+                        //value for this config property non found. Do nothing
+                    }
                 }
             }
         }
