4ce5aac5864de934938fd1289fadcc69b3acd18d
sfroee <sfroee@users.noreply.github.com>
2016-06-06 09:37:55 +0300

    Enhance BuilderSelectionStrategy (#715)
    Allow select from available builders using data from
    builder environment and build request
    
    Signed-off-by: Roy Fishman <sfroee@gmail.com>

--- platform-api/che-core-api-builder/src/main/java/org/eclipse/che/api/builder/BuilderSelectionStrategy.java
+++ platform-api/che-core-api-builder/src/main/java/org/eclipse/che/api/builder/BuilderSelectionStrategy.java
@@ -11,15 +11,34 @@
 package org.eclipse.che.api.builder;
 
 import java.util.List;
+import org.eclipse.che.api.builder.dto.BaseBuilderRequest;
 
 /**
- * Selects the 'best' SlaveBuilder from the List according to implementation. BuildQueue uses implementation of this interface fo
- * find the 'best' slave-builder for processing incoming build request. If more then one slave-builder available then BuildQueue collects
+ * Selects the 'best' SlaveBuilder from the List according to implementation. BuildQueue uses implementation of this interface to
+ * find the 'best' slave-builder for processing incoming build request. If slave-builder available then BuildQueue collects
  * them (their front-ends which are represented by SlaveBuilder) and passes to implementation of this interface. This implementation
  * should select the 'best' one.
  *
  * @author andrew00x
  */
 public interface BuilderSelectionStrategy {
+
+    /**
+     * Selects best matching {@code RemoteBuilder} that satisfies the implemented strategy
+     * @param slaveBuilders
+     * @return {@code RemoteBuilder} or {@code null} if there is no adequate selection
+     */
     RemoteBuilder select(List<RemoteBuilder> slaveBuilders);
+
+    /**
+     * Selects best matching {@code RemoteBuilder} that satisfies implemented strategy.
+     * Implemented strategy may consider both {@code BaseBuilderRequest} and {@code RemoteBuilder} properties
+     * This method is implementation optional, as selection by builder properties might be enough, and backward compatible.
+     * @param slaveBuilders
+     * @param request
+     * @return {@code RemoteBuilder} or {@code null} if there is no adequate selection
+     */
+    default RemoteBuilder select(List<RemoteBuilder> slaveBuilders, BaseBuilderRequest request){
+        return select(slaveBuilders);
+    }
 }
